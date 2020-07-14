97afe5ece2d65cbd514b2ca675248b5daa43baf8
kyubuns <kyubuns@gmail.com>
2020-07-14 20:24:26 +0900

    feat: UnityPackageManager用

---
+++ Unity/Assets/BridgeJsCore/README.txt
@@ -0,0 +1,5 @@
+# BridgeJsCoreUnity
+
+- Version 1.0.0
+- Created by kyubuns
+- https://github.com/kyubuns/BridgeJsCoreUnity/
---
+++ Unity/Assets/Editor/PackageExporter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Dev/Export Package")]
+        public static void Export()
+        {
+            var directories = new[]
+            {
+                "Assets/BridgeJsCore",
+            };
+
+            // ReSharper disable once AssignNullToNotNullAttribute
+            var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "BridgeJsCore.unitypackage");
+
+            AssetDatabase.ExportPackage(directories, outputPath, ExportPackageOptions.Recurse);
+
+            Debug.LogFormat("ExportPackage {0}", outputPath);
+        }
+    }
+}
